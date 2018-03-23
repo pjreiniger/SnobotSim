4adb016b6ed3b92510f89930eca2aa508d82a410
PJ Reiniger <pj.reiniger@gmail.com>
2018-03-22 21:39:55 -0400

    Ran formatter Adding formatter check to travis

--- snobot_sim/src/main/native/cpp/SnobotSim/MotorSim/DcMotorModelConfig.cpp
+++ snobot_sim/src/main/native/cpp/SnobotSim/MotorSim/DcMotorModelConfig.cpp
@@ -36,7 +36,7 @@ DcMotorModelConfig::DcMotorModelConfig(
                 aHasBrake,
                 aInverted,
                 aStallTorque / aStallCurrent,
-                (aFreeSpeedRpm / aNominalVoltage) * (M_PI * 2.0) / 60.0,
+                (aFreeSpeedRpm / aNominalVoltage) * (3.14159265359 * 2.0) / 60.0, // wpiformat removes M_PI
                 aNominalVoltage / aStallCurrent)
 {
 }
