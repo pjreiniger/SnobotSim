164a6b22578ed44d3bf6b4ac48d13d7616c84fc7
PJ Reiniger <pj.reiniger@gmail.com>
2018-06-26 21:15:17 -0400

    Fix pip version

--- .travis_setup_linux.sh
+++ .travis_setup_linux.sh
@@ -4,6 +4,16 @@ wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt-get update -q || true
 sudo apt-get install clang-format-5.0 -y
 
-sudo sh -c "echo 'y' | apt install python-pip"
+printf "HELLO"
+printf "\n\n\n\n"
+which pip
+which pip3
+
+sudo sh -c "echo 'y' | apt install python3-pip"
+
+printf "HELLO2222"
+printf "\n\n\n\n"
+which pip
+which pip3
 pip install --user wpiformat
 pip install --user yapf
