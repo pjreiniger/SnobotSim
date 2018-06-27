a6e8b28a75f2359f5d9425d798173c2ecdfdbe21
PJ Reiniger <pj.reiniger@gmail.com>
2018-06-27 01:31:02 -0400

    Removing debug

--- .travis_setup_linux.sh
+++ .travis_setup_linux.sh
@@ -4,17 +4,8 @@ wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt-get update -q || true
 sudo apt-get install clang-format-5.0 -y
 
-printf "HELLO"
-printf "\n\n\n\n"
-which pip
-which pip3
-
 sudo sh -c "echo 'y' | apt install python3-pip"
 sudo sh -c "apt-get install -y python3-setuptools"
 
-printf "HELLO2222"
-printf "\n\n\n\n"
-which pip
-which pip3
 pip3 install --user wpiformat
 pip3 install --user yapf
