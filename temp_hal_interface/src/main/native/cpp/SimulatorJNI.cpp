b3f1065c00d65fdd96e4ab2edda449b8bb9b8f90
PJ Reiniger <pj.reiniger@gmail.com>
2018-06-26 22:16:25 -0400

    Testing windows x86

--- temp_hal_interface/src/main/native/cpp/SimulatorJNI.cpp
+++ temp_hal_interface/src/main/native/cpp/SimulatorJNI.cpp
@@ -27,6 +27,13 @@ JNIEXPORT void JNICALL
 Java_edu_wpi_first_hal_sim_mockdata_SimulatorJNI_resetHandles
   (JNIEnv* env, jclass)
 {
+    static bool initialized = false;
+
+    if (!initialized)
+    {
+        HAL_Initialize(0, 0);
+    }
+
     SnobotSim::ResetCallbacks(env);
     hal::HandleBase::ResetGlobalHandles();
 }
