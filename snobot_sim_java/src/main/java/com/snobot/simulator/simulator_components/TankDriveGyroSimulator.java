b5c2813f756e698ed0e3ba1169fc19169f86967a
PJ Reiniger <pj.reiniger@gmail.com>
2018-07-01 01:01:55 -0400

    Moving TankDriveConfig to common directory.
    Makes it easier to switch between java and c++ version

--- snobot_sim_java/src/main/java/com/snobot/simulator/simulator_components/TankDriveGyroSimulator.java
+++ snobot_sim_java/src/main/java/com/snobot/simulator/simulator_components/TankDriveGyroSimulator.java
@@ -29,7 +29,7 @@ public class TankDriveGyroSimulator implements ISimulatorUpdater
         mRightEncoder = SensorActuatorRegistry.get().getEncoders().get(aConfig.getmRightEncoderHandle());
         mLeftEncoder = SensorActuatorRegistry.get().getEncoders().get(aConfig.getmLeftEncoderHandle());
         mGyroWrapper = SensorActuatorRegistry.get().getGyros().get(aConfig.getmGyroHandle());
-        mKP = aConfig.mTurnKp;
+        mKP = aConfig.getmTurnKp();
 
         mIsSetup = mLeftEncoder != null && mRightEncoder != null && mGyroWrapper != null;
 
@@ -69,68 +69,6 @@ public class TankDriveGyroSimulator implements ISimulatorUpdater
     public Object getConfig()
     {
         return mConfig;
-    }
-
-    public static class TankDriveConfig
-    {
-        private int mLeftEncoderHandle;
-        private int mRightEncoderHandle;
-        private int mGyroHandle;
-        private double mTurnKp;
-
-        public TankDriveConfig()
-        {
-            this(-1, -1, -1, 1);
-        }
-
-        public TankDriveConfig(int aLeftHandle, int aRightHandle, int aGyroHandle, double aTurnKp)
-        {
-            mLeftEncoderHandle = aLeftHandle;
-            mRightEncoderHandle = aRightHandle;
-            mGyroHandle = aGyroHandle;
-            mTurnKp = aTurnKp;
-        }
-
-        public int getmLeftEncoderHandle()
-        {
-            return mLeftEncoderHandle;
-        }
-
-        public void setmLeftEncoderHandle(int aLeftEncoderHandle)
-        {
-            this.mLeftEncoderHandle = aLeftEncoderHandle;
-        }
-
-        public int getmRightEncoderHandle()
-        {
-            return mRightEncoderHandle;
-        }
-
-        public void setmRightEncoderHandle(int aRightEncoderHandle)
-        {
-            this.mRightEncoderHandle = aRightEncoderHandle;
-        }
-
-        public int getmGyroHandle()
-        {
-            return mGyroHandle;
-        }
-
-        public void setmGyroHandle(int aGyroHandle)
-        {
-            this.mGyroHandle = aGyroHandle;
-        }
-
-        public double getmTurnKp()
-        {
-            return mTurnKp;
-        }
-
-        public void setmTurnKp(double aTurnKp)
-        {
-            this.mTurnKp = aTurnKp;
-        }
-
     }
 
 }
