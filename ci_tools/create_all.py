8c170567bd717480d0bf1b85281713b9d2005cb1
PJ Reiniger <pj.reiniger@gmail.com>
2018-11-05 22:10:47 -0500

    Formatting, disable linux

--- ci_tools/create_all.py
+++ ci_tools/create_all.py
@@ -1,4 +1,3 @@
-
 import os
 import shutil
 import zipfile
@@ -11,25 +10,33 @@ def createAll(start_dir, root_path, library_name, version):
 
     natives = []
     # natives.append(os.path.join(library_path, library_name + "-" + version + "-linux-x86-64.jar"))
-    natives.append(os.path.join(library_path, library_name + "-" + version + "-os x-x86-64.jar"))
-    natives.append(os.path.join(library_path, library_name + "-" + version + "-windows-x86.jar"))
-    natives.append(os.path.join(library_path, library_name + "-" + version + "-windows-x86-64.jar"))
+    natives.append(
+        os.path.join(library_path, library_name + "-" + version +
+                     "-os x-x86-64.jar"))
+    natives.append(
+        os.path.join(library_path, library_name + "-" + version +
+                     "-windows-x86.jar"))
+    natives.append(
+        os.path.join(library_path, library_name + "-" + version +
+                     "-windows-x86-64.jar"))
 
     for native in natives:
         zip_ref = zipfile.ZipFile(native, 'r')
         zip_ref.extractall(temp_build_dir)
         zip_ref.close()
 
-
     # ziph is zipfile handle
-    ziph = zipfile.ZipFile(library_path + "/" + library_name + "-" + version + "-all.jar", 'w', zipfile.ZIP_DEFLATED)
+    ziph = zipfile.ZipFile(
+        library_path + "/" + library_name + "-" + version + "-all.jar", 'w',
+        zipfile.ZIP_DEFLATED)
     for root, dirs, files in os.walk(temp_build_dir):
         for file in files:
             full_path = os.path.join(root, file)
-            zip_path = full_path[len(temp_build_dir)+len(os.sep):] #XXX: relative path
-            print (full_path + ", " + zip_path)
+            zip_path = full_path[
+                len(temp_build_dir) + len(os.sep):]  #XXX: relative path
+            print(full_path + ", " + zip_path)
             ziph.write(full_path, zip_path)
-            
+
     shutil.rmtree(temp_build_dir)
 
 
@@ -47,16 +54,18 @@ def detect_version(start_dir):
 
 
 def debug(start_dir):
-    print ("Start Dir : " + start_dir)
-    print ("Start Dir : "  + str(os.listdir(start_dir)))
-    print ("CWD       : " + os.getcwd())
-    print ("UP        : "  + str(os.listdir(os.getcwd() + "/..")))
-
+    print("Start Dir : " + start_dir)
+    print("Start Dir : " + str(os.listdir(start_dir)))
+    print("CWD       : " + os.getcwd())
+    print("UP        : " + str(os.listdir(os.getcwd() + "/..")))
 
 
 def main():
     parser = argparse.ArgumentParser(description='Create combined library')
-    parser.add_argument('--start_dir', help='an integer for the accumulator', default=os.path.abspath("../build/"))
+    parser.add_argument(
+        '--start_dir',
+        help='an integer for the accumulator',
+        default=os.path.abspath("../build/"))
     args = parser.parse_args()
 
     start_dir = args.start_dir + "/"
