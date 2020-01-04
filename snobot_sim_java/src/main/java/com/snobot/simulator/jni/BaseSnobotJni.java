72089139bcdb675445eedc2626698cbdc02f38b2
PJ Reiniger <pj.reiniger@gmail.com>
2020-01-03 21:00:06 -0500

    Starting to replace

--- snobot_sim_java/src/main/java/com/snobot/simulator/jni/BaseSnobotJni.java
+++ snobot_sim_java/src/main/java/com/snobot/simulator/jni/BaseSnobotJni.java
@@ -8,11 +8,13 @@ public class BaseSnobotJni
 {
     static
     {
+        
+        JniLibraryResourceLoader.loadLibrary("wpiutiljni");
         JniLibraryResourceLoader.loadLibrary("wpiutil");
         JniLibraryResourceLoader.loadLibrary("wpiHal");
-        JniLibraryResourceLoader.loadLibrary("halsim_adx_gyro_accelerometer");
-        JniLibraryResourceLoader.loadLibrary("navx_simulator");
-        JniLibraryResourceLoader.loadLibrary("adx_family");
+        // JniLibraryResourceLoader.loadLibrary("halsim_adx_gyro_accelerometer");
+        // JniLibraryResourceLoader.loadLibrary("navx_simulator");
+        // JniLibraryResourceLoader.loadLibrary("adx_family");
         JniLibraryResourceLoader.loadLibrary("CTRE_PhoenixCCI");
         JniLibraryResourceLoader.loadLibrary("SparkMaxDriver");
 
---
+++ snobot_sim_java/src/main/java/com/snobot/simulator/simulator_components/adx_family/LazySimDoubleWrapper.java
@@ -0,0 +1,21 @@
+
+package com.snobot.simulator.simulator_components.adx_family;
+
+public class LazySimDoubleWrapper
+{
+    public LazySimDoubleWrapper(String aDeviceName, String aValueName)
+    {
+
+    }
+
+    public double get()
+    {
+        return 0;
+    }
+
+    public void set(double aValue)
+    {
+        
+    }
+
+}
